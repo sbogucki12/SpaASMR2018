f4eab0e8eaec1e909809bbc0de12d55fd720daf8
Steve Bogucki <sbogucki@mail.usf.edu>
2018-03-07 19:27:19 -0800

    seeded Videos table with ASMRRequests videos

---
+++ SpaASMR2018/SpaASMR2018/Migrations/201803080258174_SeedVideosAsmrRequests.cs
@@ -0,0 +1,72 @@
+namespace SpaASMR2018.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedVideosAsmrRequests : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('An Esthetician Visit  ASMR  Personal Attention Face Touching Face Tapping', 'https://www.youtube.com/watch?v=ZJIQJXfXILU', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR | Tapping on Pretty Things + Giveaway!', 'https://www.youtube.com/watch?v=W2_OQFQ_T8c', 2, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Therapist Role Play + BetterHelp Online Therapy', 'https://www.youtube.com/watch?v=mFyGX5CTl7w&t=722s', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Aqua Sounds Ear to Ear (Water Spraying Sloshing and Fizzing)', 'https://www.youtube.com/watch?v=GLrzKf24MAc', 2, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Dont forget its okay to remember! Memory Restoration Roleplay for Sleep', 'https://www.youtube.com/watch?v=Pd6Bf5C-smw', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR For Guiding You To Sleep & Calming Your Anxiety', 'https://www.youtube.com/watch?v=zL8wdYkQgBU&t=16s', 1, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Unique Brushes On Your Ears', 'https://www.youtube.com/watch?v=vg-dHXGGZJ4', 2, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Cologne & Perfume Shop Roleplay', 'https://www.youtube.com/watch?v=5bF2aU2EuHI', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Steamy Cold Relief ASMR', 'https://www.youtube.com/watch?v=AjEf2aBt1ns&t=1401s', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Lymphatic Massage - ASMR Medical Role Play', 'https://www.youtube.com/watch?v=wdLB3udjOR0', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Relaxing Laser Hair Removal Session - ASMR Role Play', 'https://www.youtube.com/watch?v=iq4A2NhuAiE', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Sudsy Hair Wash n Dry ASMR - Shampoo Scalp Massage & Blow Dry for Relaxation', 'https://www.youtube.com/watch?v=x018RC5hJWo', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Fizzy Cloud Dough ASMR (Fully Whispered Binaural Fizzing)', 'https://www.youtube.com/watch?v=_egmgBcYU70', 2, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('The Herb Shoppe', 'https://www.youtube.com/watch?v=_4h41VWPyeo', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMRniversary Spa Day Treatment #3 of 3 - Deep Sleep Treatment', 'https://www.youtube.com/watch?v=EAMHqNxa9yY', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMRniversary Spa Day Treatment #2 of 3 - Exfoliating Hydra Facial', 'https://www.youtube.com/watch?v=ABNFZMWZE6s', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMRniversary Spa Day Treatment #1 of 3 - Relaxing Scalp & Ear Massage ', 'https://www.youtube.com/watch?v=vMcqVGrQxZs&t=292s', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('All Up In Your Ears 2 - ASMR (Ear Brushing Ear Cupping Water Dropper Ear to Ear Soft Speaking)', 'https://www.youtube.com/watch?v=RlUAB8SxX0g', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('You are the Evidence: A Forensic Investigation Binaural ASMR Role Play', 'https://www.youtube.com/watch?v=BCreGNTEm1c', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('The K3YS: Time - A Binaural ASMR Role Play', 'https://www.youtube.com/watch?v=BRrHVXGT7tg', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Let Me Restore You - An Art Restoration Binaural ASMR Role Play', 'https://www.youtube.com/watch?v=lwQN1jr7NtY', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Raw Tingles - Holiday Ornaments - (Binaural ASMR Soft Spoken Tapping, Scratching)', 'https://www.youtube.com/watch?v=0v3DPwqBMmY', 2, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Remember its okay to forget! Memory Erasure Role Play', 'https://www.youtube.com/watch?v=vfVltlT-Rj0&t=5s', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Luxury Beach Cabana Role Play', 'https://www.youtube.com/watch?v=RC9oAtHYWs8&t=3s', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('FX Make-up Artist Role Play', 'https://www.youtube.com/watch?v=xK8oQtdm9uM', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Thrifty Tingles: Poker Chips & Giant Cards', 'https://www.youtube.com/watch?v=-by_KstAsgM', 2, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Raw Tingles: Effervescent Tabs Plastic Grass Ball Craft Fur', 'https://www.youtube.com/watch?v=nY2bqqcFywA', 2, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('An Esthetician Visit - Binaural Role Play', 'https://www.youtube.com/watch?v=LMs-VIbc_48', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Raw Tingles: All Up In Your Ears (Ear Massage Ear Cupping Pom Pom Wood Spheres)', 'https://www.youtube.com/watch?v=k-VVYMsBFXM&t=102s', 1, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('A Quiet Vlog: Underwater Suds', 'https://www.youtube.com/watch?v=uX6aqoP-Wzw', 1, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Physical Therapist Role Play', 'https://www.youtube.com/watch?v=VsZMFrup_lc&t=16s', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Raw Tingles: Shaving Cream Goozerk Gelcap Vitamins', 'https://www.youtube.com/watch?v=MP7pszZWKfM', 2, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Tutorial Tingles: Departure Ep. 2 Make-up & Stipple', 'https://www.youtube.com/watch?v=cyfJpUUwUS4', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Thrifty Tingles: Animal Salt & Pepper Set & Chess Set', 'https://www.youtube.com/watch?v=q77PRmJmzUs', 2, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Raw Tingles: Miniatures Peel Off Mask Rice Krispies', 'https://www.youtube.com/watch?v=Cv9zHo3Bzb8', 2, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Submerged Q&A / Food Coloring Experiment - Binaural ASMR', 'https://www.youtube.com/watch?v=qewt9fxPJdg', 1, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Tutorial Tingles: My Everyday Makeup Look', 'https://www.youtube.com/watch?v=zzfyi7sW044', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Departure Ep. 2: Frontiers - ASMR Sci-Fi Series', 'https://www.youtube.com/watch?v=YYpI8847naw', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('12 Days of Role Plays: Day 11 - Binaural Spa Facial & Scalp Massage', 'https://www.youtube.com/watch?v=xlQOA0EKpoc', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('12 Days of Role Plays: Day 10 - House Cleaning Service', 'https://www.youtube.com/watch?v=G4plfpY6D-w', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('12 days of Role Plays: Day 7 - Binaural Clothes Fitting', 'https://www.youtube.com/watch?v=aFQjA3WX40Y', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('12 Days of Role Plays: Day 5 - Binaural Cranial Nerve Exam', 'https://www.youtube.com/watch?v=dzUxsSCR13E', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('12 Days of Role Plays: Day 4 - A Tooth Fairy Teeth Cleaning', 'https://www.youtube.com/watch?v=0D4f3Hjqvm8', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('12 Days of Role Plays: Day 3 - Candy Shop', 'https://www.youtube.com/watch?v=2kZRQbZOHzQ', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('12 Days of Role Plays: Day 2 - Binaural Haircut', 'https://www.youtube.com/watch?v=qyGV0TOsrmQ', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Time Travel Tuesday: Nirvana', 'https://www.youtube.com/watch?v=eNMpT_Stc1U', 1, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Time Travel Tuesday: Bob Ross', 'https://www.youtube.com/watch?v=2JAz7698sa4', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Tooth Fairy Tingles', 'https://www.youtube.com/watch?v=kRyhEISY4PU', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Thrifty Thursday: Face Brushing', 'https://www.youtube.com/watch?v=_1GiRBZQbPw', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Departure Ep. 1: Departure (or Space Travel Agent) ', 'https://www.youtube.com/watch?v=oapgiZc5i-g&t=5s', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Shipping Service Role Play; Bubble Wrapping Ryan Goslings Jacket', 'https://www.youtube.com/watch?v=KOgeIp2jHE0', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Spa Tour - Choose Your Own Adventure - Role Play', 'https://www.youtube.com/watch?v=k9HXt6YKH_I', 3, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Cranial Nerve Exam', 'https://www.youtube.com/watch?v=Ae-c73J39KM', 4, 8)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Mens Hair Stylist - Shampoo Cut & Style', 'https://www.youtube.com/watch?v=5F98uA4jNmM', 4, 8)");
+
+
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
