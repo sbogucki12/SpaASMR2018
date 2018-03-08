0b29fc34aee9bc23e7bd55260baa49f61bff0e32
Steve Bogucki <sbogucki@mail.usf.edu>
2018-03-07 18:40:28 -0800

    seeded videos table with heather feather videos

---
+++ SpaASMR2018/SpaASMR2018/Migrations/201803080205254_SeedHeatherFeatherVideos.cs
@@ -0,0 +1,70 @@
+namespace SpaASMR2018.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedHeatherFeatherVideos : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural Sandscript Automatic Kinetic Art Sand Drawing Machine', 'https://www.youtube.com/watch?v=FtAy1IPzkRQ', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Intense ASMR Binaural Ear to Ear Whispering For Tingles Relaxation and Sleep', 'https://www.youtube.com/watch?v=s45bUg8x2u4&t=231s', 1, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Pump You Up Full Of Binaural ASMR Tingles', 'https://www.youtube.com/watch?v=xI0lapOSIJA&t=776s', 1, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Spraying You With Binaural ASMR Tingles', 'https://www.youtube.com/watch?v=tDojcTfrQ-A&t=189s', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural Ear Shaving So You Can Sleep Tingle and Relax.', 'https://www.youtube.com/watch?v=yqxRQbp__oo', 3, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural WoodWick Candles For Tingles Sleep and Relaxation', 'https://www.youtube.com/watch?v=MJqtySKbWJs', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural Face Paint Role Play: Facts and Tingles for Relaxation and Sleep', 'https://www.youtube.com/watch?v=ckqg1mZFQjo&t=396s', 3, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural Feather Flight Attendant Role Play For Relaxation and Sleep', 'https://www.youtube.com/watch?v=gDJOM_DhpG4', 3, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('9 Binaural ASMR Triggers To Keep You Tingling', 'https://www.youtube.com/watch?v=9S67kKtCQzk', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Ferrofluid ASMR: Playing with Rocket Fuel! Oddly Satisfying Magnetic Liquid Magic', 'https://www.youtube.com/watch?v=KUwQjteNsP4&t=164s', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural ASMR: Ear to Ear Whispering Interesting Facts For Relaxation Sleep and Tingles', 'hhttps://www.youtube.com/watch?v=ELR4n_L-brU&t=270s', 1, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('THIS. IS. SPATA! Binaural ASMR Spa Role Play With Cupping Therapy Facial Steam And Massage', 'https://www.youtube.com/watch?v=5TIEzpYTHbU', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Watch Behind Your Ears: 90 Minutes Of Binaural ASMR Ear Treatment Sounds For Relaxation And Sleep', 'https://www.youtube.com/watch?v=7nOoyRvW6dk&t=1940s', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Extremely Satisfying ASMR: Binaural Kinetic Sand Play For Relaxation Tingles And Sleep', 'https://www.youtube.com/watch?v=CSvYyjfYEok', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Ear Cleaning Role Play: Binaural Audio That Lets You Feel Me In Your Ears!', 'https://www.youtube.com/watch?v=eTzGx5n7qpk', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural Tingle Blitz: Sage Stick aka Smudge Stick Crackling and Soft Speaking For Relaxation', 'https://www.youtube.com/watch?v=_QTtcist2Ck', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural Tingle Blitz: Shaving Lather For Relaxation', 'https://www.youtube.com/watch?v=JSg_FPPrjKM', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural Tingle Blitz: Princess Ducky And Blowing In Your Ears For Tingles and Relaxation', 'https://www.youtube.com/watch?v=9jAPtJsskRw', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural ASMR Tingle Blitz: Adventures With Silicone For Tingles And Relaxation', 'https://www.youtube.com/watch?v=mN3eO93ez6Q', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural Twin ASMR Tingle Blitz: Blowing In Your Ears. You Can Feel My Breath In Your Ears!', 'https://www.youtube.com/watch?v=verF7vNJp-Y', 1, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural Tingle Blitz: Rubber Fingertips', 'https://www.youtube.com/watch?v=8YhHUHqOi7E', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural Ear Examination Role Play: Doctor Feather will see you now!', 'https://www.youtube.com/watch?v=Kmp0oKzNG4s&t=22s', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural ASMR Experiment: Shaving Cream And Pop Rocks', 'https://www.youtube.com/watch?v=d97Ru0QDskM', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural Steamy Facial Treatment Role Play', 'https://www.youtube.com/watch?v=eaTs_wYWj6o&t=82s', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Ear Therapy ASMR: Featuring Binaural Ear Tapping Ear Massage And Ear Cupping For Relaxation', 'https://www.youtube.com/watch?v=XCxm2FqWs10', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Microphone Test Fest: Over 90 Minutes of Binaural and Stereo Triggers Using 9 Microphones', 'https://www.youtube.com/watch?v=CF9pGTE4i-o', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Ultimate Head and Scalp Massage Role Play: Realistic Binaural Sounds For Relaxation', 'https://www.youtube.com/watch?v=mWnpMTCtNSc', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('The K3YS: Courage - A Binaural ASMR Role Play', 'https://www.youtube.com/watch?v=rmxStVar3AA', 3, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('8 New Binaural ASMR Triggers To Tingle Relax And Sleep', 'https://www.youtube.com/watch?v=sWzGQ_9rOFg', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural Allergy Skin Test ASMR Role Play: A Medical Examination For Relaxation', 'https://www.youtube.com/watch?v=L46nYQLLvrc&t=536s', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('The Top 120 Binaural ASMR Triggers: A 4.5 Hour Tingle Fest!', 'https://www.youtube.com/watch?v=7GwtmUnecII', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural Doctor Examination Role Play: You Need A Check Up! (For Tingles Relaxation & Sleep)', 'https://www.youtube.com/watch?v=OjN3w6iU3Qk&t=56s', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Testing Your Alignment: Binaural ASMR Doctor Examination With Your Shoulder Angel And Devil', 'https://www.youtube.com/watch?v=qHcdGbgu5IE', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural Harmonic Massage Role Play: ASMR using Smudging, Reiki Tuning Forks & Scalp Massage', 'https://www.youtube.com/watch?v=eOh-CB8u1QY', 2, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural Esthetician ASMR Role Play: Skin Treatment, Facial & Scalp Massage From Twin Feathers Spa', 'https://www.youtube.com/watch?v=gsqzRWFUEVo', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural ASMR: Ear Massage & Ear Covering Marathon With Ear To Ear Whispering About Disney Secrets', 'https://www.youtube.com/watch?v=xFyJI4bzvaY', 1, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural ASMR: Youre a Treasure Role Play For Relaxation', 'https://www.youtube.com/watch?v=IsBjJX06V8c', 3, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural ASMR: Up Close And Up On Your Ears For Relaxation', 'https://www.youtube.com/watch?v=tHu2Sk0DJpc', 1, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural Medical Role Play: A Snake Oil Salesman for Relaxation', 'https://www.youtube.com/watch?v=uOluZxHIALw', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Ear Nose and Throat Examination (ENT) Role Play: A Binaural Medical Exam For Relaxation', 'https://www.youtube.com/watch?v=OO3f9cB0_r4', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural ASMR Boutique: Fragrance Counter (Perfume And Cologne) Role Play For Relaxation', 'https://www.youtube.com/watch?v=ytlppD2KfsE', 3, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural ASMR: The Sleep Police Role Play For Tingles, Relaxation And Of Course---Sleep', 'https://www.youtube.com/watch?v=nuJDv4ahiec', 3, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR SCIENCE Role Play: Testing Your 5 Senses for Relaxation', 'https://www.youtube.com/watch?v=_6aBbQx6cxE', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Sound Slice: Binaural SKSK (& the origin of SK) and Ear Massage for Relaxation', 'https://www.youtube.com/watch?v=rOGzMJZFODM', 1, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Brushing Your Face And Ears With A Makeup Brush Assortment For Relaxation', 'https://www.youtube.com/watch?v=vO5SF6xSW3U', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural (3D) Cranial Nerve Examination Role Play for Tingles Relaxation and Sleep', 'https://www.youtube.com/watch?v=BJ8EBBTEb0o', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Eye Examination Role Play for Relaxation', 'https://www.youtube.com/watch?v=v74ceeUarG8', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural/Stereo Ear Exam and Ear Cleaning Role Play for Relaxation and Sleep', 'https://www.youtube.com/watch?v=wQiS_vYI9iM', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Binaural (3D) Comprehensive Physical Examination Role Play for Relaxation', 'https://www.youtube.com/watch?v=SaMWjFLWZoo', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Science Role Play for Relaxation', 'https://www.youtube.com/watch?v=BG62f4s1aHM', 3, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Eye Examination Role Play for Relaxation (ASMR)', 'https://www.youtube.com/watch?v=2ehJV76wcIc', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Comprehensive Physical Examination Role Play for Relaxation (ASMR)', 'https://www.youtube.com/watch?v=WWMNcb3WQJY', 4, 7)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Relaxing Cranial Nerve Examination Role Play (Soft Spoken ASMR)', 'https://www.youtube.com/watch?v=JWZCbq7ITyE', 4, 7)");
+            
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
