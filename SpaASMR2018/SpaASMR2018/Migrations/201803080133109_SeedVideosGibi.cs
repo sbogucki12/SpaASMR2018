35e302b7c4f66d03cb7fdb10762568fcfdc35d16
Steve Bogucki <sbogucki@mail.usf.edu>
2018-03-07 18:03:22 -0800

    seeded video table with gibi videos

---
+++ SpaASMR2018/SpaASMR2018/Migrations/201803080133109_SeedVideosGibi.cs
@@ -0,0 +1,77 @@
+namespace SpaASMR2018.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedVideosGibi : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('SLOW Tapping & Whispered Trigger Assortment', 'https://www.youtube.com/watch?v=zNL_kGO6lQ8&t=1077s', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Makeup 101 - For the Beginners', 'https://www.youtube.com/watch?v=O2bTJ-TzDsY', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Flu Doctor Checkup - Medical Roleplay', 'https://www.youtube.com/watch?v=YRLlzsExRU4', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('5 Different Slime Triggers', 'https://www.youtube.com/watch?v=zNL_kGO6lQ8&t=1077s', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Treat Yourself  Beauty Product Trigger Assortment', 'https://www.youtube.com/watch?v=RbAYpgLOgj4', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('ASMR Sleep & Relaxation Treatment', 'https://www.youtube.com/watch?v=7FHHXTUsJx0&t=99s', 1, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Daisy Cuts and Dyes Your Hair', 'https://www.youtube.com/watch?v=-KIz0tTo51E', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('The Top 10 Triggers for ASMR  Sleep', 'https://www.youtube.com/watch?v=BuACWFJ1oOY&t=104s', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Sketching and Coloring Your Portrait', 'https://www.youtube.com/watch?v=1wcMcqkPwfo', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Tapping with Fingertips ( Binaural Ear-to-Ear Whispers)', 'https://www.youtube.com/watch?v=SjeB-p_h1Fg', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Life is Strange  Chloe Roleplay', 'https://www.youtube.com/watch?v=HJKHU3jU2sc&t=788s', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Up-Close Elf Ear Measuring Roleplay', 'https://www.youtube.com/watch?v=kbx-qZnTr-0', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Zelda Roleplay - Legends & Potions', 'https://www.youtube.com/watch?v=L3DdChuw6uk&t=32s', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Big Sister Gives You a Spa Treatment', 'https://www.youtube.com/watch?v=rxo_Kq2eJMk&t=190s', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Cranial Nerve Examination by Daisy', 'https://www.youtube.com/watch?v=Lbboz1s8uEI&t=35s', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Face Touching & Hand Movements for Sleep', 'https://www.youtube.com/watch?v=5igB7uDN4Tc&t=684s', 1, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Binaural Tapping on Glass Objects', 'https://www.youtube.com/watch?v=fBFEykWKAZQ', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Welcome Aboard the Spaceship! [Sci-Fi Firefly/Serenity Roleplay]', 'https://www.youtube.com/watch?v=3NfuWqGtpkc', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Artificial Intelligence Doctor Clinic', 'https://www.youtube.com/watch?v=-lKebOXSQh8', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Tapping on Wooden Objects', 'https://www.youtube.com/watch?v=snu2c5u8p_8', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Relaxing Facial and Skin Treatment Spa', 'https://www.youtube.com/watch?v=PJnxO3BFo88', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Tattoo Consultation & Trial', 'https://www.youtube.com/watch?v=-SbxVpNwYvU', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Mechanical Keyboard Typing and Binaural Close Whispers', 'https://www.youtube.com/watch?v=x8On2wSH2FQ', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Classroom Teacher Roleplay', 'https://www.youtube.com/watch?v=acUwaifsx6k', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Soft Face Touching | Ear Scratching for Sleep', 'https://www.youtube.com/watch?v=yFJ82hycfRI', 1, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('International First Class Flight Attendant Roleplay', 'https://www.youtube.com/watch?v=dk5POe042DA&t=324s', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Pure Tapping & Triggers', 'https://www.youtube.com/watch?v=zFCZLIDs-v4', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Tapping on Books Page Turning', 'https://www.youtube.com/watch?v=zNL_kGO6lQ8&t=1077s', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Sleep Clinic Roleplay', 'https://www.youtube.com/watch?v=19nvOC1Cu_c', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Positive Affirmations & Thoughts', 'https://www.youtube.com/watch?v=rNam7W4Df64', 1, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Painting Your Face!', 'https://www.youtube.com/watch?v=zNL_kGO6lQ8&t=1077s', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Scratching Tracing Tapping Trigger Assortment', 'https://www.youtube.com/watch?v=AxmYZ67h_88', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Doing Your Eyebrows', 'https://www.youtube.com/watch?v=LtndcrtTKRY', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Keyboard Sounds Typing & Whispering', 'https://www.youtube.com/watch?v=ApVB_aWzOXk', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Pure Finger Fluttering - One Hour', 'https://www.youtube.com/watch?v=ZYcXo-Njeks', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Spa Facial Roleplay', 'https://www.youtube.com/watch?v=ApVB_aWzOXk', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Getting You Ready for the Anime Convention Roleplay!', 'https://www.youtube.com/watch?v=6rrxMLH6l6I', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Daisy & Dr. Gibi Treat Your Anxiety - Airport Roleplay', 'https://www.youtube.com/watch?v=KhWM2EhmdEo', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Face Examination Roleplay - With Gloves', 'https://www.youtube.com/watch?v=tyAP__jBeg0', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Scalp Massage & Hair Brush + Treatment Roleplay', 'https://www.youtube.com/watch?v=UvXRKnyAPV4', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Testing 3 Brushes On You For Sleep', 'https://www.youtube.com/watch?v=Fm4I90zQ6HI', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Mic Scratching | Whisper Ramble', 'https://www.youtube.com/watch?v=tJevSV3tGzM', 1, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Water Bottle Spraying Tapping Sounds!', 'https://www.youtube.com/watch?v=8khctdmMBCo', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Rubber & Latex Gloves', 'https://www.youtube.com/watch?v=XOJFnmWoivo', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Luxury Hotel Roleplay - Daisy Checks You In', 'https://www.youtube.com/watch?v=iKRDCAbLuXM', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Magic Eraser', 'https://www.youtube.com/watch?v=OeI9YSejIKc', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('The Dentist Roleplay', 'https://www.youtube.com/watch?v=-T_f1H-hTwg', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Hospital Roleplay - Full Checkup', 'https://www.youtube.com/watch?v=t3-jZU3kxLE', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Legend of Korra Roleplay - Healing You', 'https://www.youtube.com/watch?v=W6fsSGnyCHw', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Scratching Hour', 'https://www.youtube.com/watch?v=nK8F0TGIrgM', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Teacher Roleplay (Multilingual Whisper)', 'https://www.youtube.com/watch?v=xiAQaZfqKMc', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Finger Flutter & Tapping Compilation', 'https://www.youtube.com/watch?v=crfuRhVRuKc', 2, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('First Class Passenger - Flight Attendant Roleplay', 'https://www.youtube.com/watch?v=wwqnRZ7GMTQ', 3, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Tweezer Sounds Personal Attention - Touching & Tweezing Your Face!', 'https://www.youtube.com/watch?v=swxbmcR2zKo', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Shave & Hair Trim Roleplay', 'https://www.youtube.com/watch?v=phKr4h5ceus&t=11s', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('The Cranial Nerve Exam', 'https://www.youtube.com/watch?v=B1NXNlXFViY', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Allergy Test & Skin Treatment', 'https://www.youtube.com/watch?v=jicQpYSHtVo', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Doctor Roleplay - Yearly Check-up', 'https://www.youtube.com/watch?v=kYueR4HLhyk', 4, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Ear to Ear Close Whispering', 'https://www.youtube.com/watch?v=kDwOYXRzETY', 1, 6)");
+            Sql("INSERT INTO Videos (Name, Url, VideoGenreId, ArtistId) VALUES ('Eye Exam (Nurse Roleplay!)', 'https://www.youtube.com/watch?v=6e6_ziEVsK4&t=112s', 4, 6)");
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
